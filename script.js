ad40e84a7e0c2f5bea126a64449035a68bdc4349
TamerlanMustafayev <tamerlan.m.2000@gmail.com>
2024-07-07 16:54:58 -0400

    Exercise 7. I wrote this code with plain math formula. I believe it could be written in many other different ways too

--- script.js
+++ script.js
@@ -96,8 +96,27 @@ Example: maxOfThree(17, 4, 9) should return 17.
 Complete the exercise in the space below:
 */
 
-function maxOfThree(num1, num2, num3) {
-    return Math.max(num1, num2, num3)
+// function maxOfThree(num1, num2, num3) {
+//     return Math.max(num1, num2, num3)
+// }
+
+// console.log('Exercise 6 Result:', maxOfThree(5, 10, 18));
+
+
+/*
+Exercise 7: calculateTip()
+
+Create a function called calculateTip. It should take two arguments: 
+the bill amount and the tip percentage (as a whole number). 
+The function should return the amount of the tip.
+
+Example: calculateTip(50, 20) should return 10.
+
+Complete the exercise in the space below:
+*/
+function calculateTip(billAmount, tipPercentage) {
+    return (billAmount*tipPercentage)/100
 }
 
-console.log('Exercise 6 Result:', maxOfThree(5, 10, 18));
+
+console.log('Exercise 7 Result:', calculateTip(150, 20));
